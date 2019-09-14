6a4b5f625ac11c96fe6554999d1daae94e054c5b
Cody Barr <cody.barr@gmail.com>
2019-09-14 11:36:57 -0400

    Initial commit 🤘🏻

---
+++ questions.js
@@ -0,0 +1,96 @@
+const QUESTIONS = [
+	{
+		text: 'How much health do you start with in a Hearthstone game?',
+		answers: [
+			{ text: '10', correct: false },
+			{ text: '30', correct: true },
+			{ text: '50', correct: false },
+			{ text: '100', correct: false }
+		]
+	},
+	{
+		text: 'How many cards are in a standard deck?',
+		answers: [
+			{ text: '30', correct: true },
+			{ text: '40', correct: false },
+			{ text: '10', correct: false },
+			{ text: '70', correct: false }
+		]
+	},
+	{
+		text:
+			'Which of the following is <em>NOT</em> a standard Hearthstone class?',
+		answers: [
+			{ text: 'Witch', correct: true },
+			{ text: 'Warlock', correct: false },
+			{ text: 'Shaman', correct: false },
+			{ text: 'Druid', correct: false }
+		]
+	},
+	{
+		text: 'Which ability allows a minion to attack the turn it is played?',
+		answers: [
+			{ text: 'Battlecry', correct: false },
+			{ text: 'Windfury', correct: false },
+			{ text: 'Deathrattle', correct: false },
+			{ text: 'Charge', correct: true }
+		]
+	},
+	{
+		text:
+			'Which of the following is <em>NOT</em> a standard card type in Hearthstone?',
+		answers: [
+			{ text: 'Minion', correct: false },
+			{ text: 'Spell', correct: false },
+			{ text: 'Artifact', correct: true },
+			{ text: 'Weapon', correct: false }
+		]
+	},
+	{
+		text: 'Cards can be found in packs or crafted using what resource?',
+		answers: [
+			{ text: 'Gold', correct: false },
+			{ text: 'Crystals', correct: false },
+			{ text: 'Dust', correct: true },
+			{ text: 'Gil', correct: false }
+		]
+	},
+	{
+		text: 'Each card pack in Hearthstone has how many cards?',
+		answers: [
+			{ text: '7', correct: false },
+			{ text: '10', correct: false },
+			{ text: '3', correct: false },
+			{ text: '5', correct: true }
+		]
+	},
+	{
+		text:
+			'Which of the following is <em>NOT</em> a card rarity in Hearthstone?',
+		answers: [
+			{ text: 'Legendary', correct: false },
+			{ text: 'Epic', correct: false },
+			{ text: 'Common', correct: false },
+			{ text: 'Uncommon', correct: true }
+		]
+	},
+	{
+		text:
+			'Every card pack is guaranteed to have at least one card of the following rarity (or higher):',
+		answers: [
+			{ text: 'Legendary', correct: false },
+			{ text: 'Rare', correct: true },
+			{ text: 'Epic', correct: false },
+			{ text: 'Common', correct: false }
+		]
+	},
+	{
+		text: 'What is the reward for winning 500 ranked play games?',
+		answers: [
+			{ text: '1000 Gold', correct: false },
+			{ text: 'A Legendary card', correct: false },
+			{ text: '5 card packs', correct: false },
+			{ text: 'A golden hero portrait', correct: true }
+		]
+	}
+]
